746c2466f5d8ec1a857da81e6d553e4e5119f957
gaotian52 <gaotian52@gmail.com>
2023-01-13 17:30:14 -0600

    r1_0103

--- scripts/figure_script.R
+++ scripts/figure_script.R
@@ -87,84 +87,41 @@ fig_1a <- ggplot() +
   scale_y_continuous( expand = c(0, 0) ,name = expression(-log[10](italic(p))), 
                       sec.axis = sec_axis( trans=~./10, name="Mean percentile of\nsignificant STRs / 20 kb") )+
   facet_grid(.~str_genotype,scales="free")
+  
+
+
+perc_sig <- factoral_eSTR_lrt_log %>% 
+  dplyr::filter(str_genotype=="STR genotype" & nSTR_pheno==6)
+  dplyr::ungroup() %>% 
+  dplyr::mutate(sig=ifelse(real_lrt_p > bf_thres,"pass","no")) %>% 
+  dplyr::group_by(nSTR_pheno,sig) %>% 
+  dplyr::count() %>% 
+  tidyr::spread(sig,n) %>% 
+  dplyr::mutate(sig_perc=100*pass/(pass+no))
+
+
+
+
+
+
+
  
-
-fig_1a <- ggplot() +
-#  geom_point(data=factoral_eSTR_lrt_df, alpha=0.8,size=0.2,shape=20, aes(x=distance,y=padj,color=type)) +
-  geom_point(data=subset(factoral_eSTR_lrt_log, nSTR_pheno %in% c(2,3)), alpha=0.8,size=0.2,shape=20, aes(x=distance,y=real_lrt_p,color=factor(nSTR_pheno))) +
-  geom_point(data=subset(factoral_eSTR_lrt_log, nSTR_pheno %in% c(4,5,6,7)), alpha=0.8,size=0.2,shape=20, aes(x=distance,y=real_lrt_p,color=factor(nSTR_pheno)))+
-  geom_point(data=factoral_eSTR_lrt_log, alpha=0.8,size=0.2,shape=20, aes(x=distance,y=perm_lrt_p ),color="black") +
-  geom_line(data=perc_eSTR,  aes(x=distance,y=padj),color="darkorange1",size=1)+
- # scale_color_manual(values=c("darkgray","lightskyblue2")) + 
+fig_S1_new <- ggplot() +
+  geom_point(data=factoral_eSTR_lrt_log, alpha=0.8,size=0.2,shape=20, aes(x=distance,y=real_lrt_p ,color=factor(nSTR_pheno)))  +
   geom_hline(data=perc_eSTR,aes(yintercept = bf_thres),
-             color = "red",
+             color = "black",
              linetype=3 )+
-  theme_cust+
-  theme(legend.position = "none") +
-  labs(x="Distance to TSS (Mb)", 
-       color="")+
-  theme(legend.position = "none", panel.spacing = unit(2,"line")) + 
-  scale_x_continuous(expand = c(0 , 0.01) ) + 
-  scale_y_continuous( expand = c(0, 0) ,name = expression(-log[10](italic(p))), 
-                      sec.axis = sec_axis( trans=~./10, name="Mean percentile of\nsignificant STRs / 20 kb") )+
-  facet_grid(.~str_genotype,scales="free")+
-  scale_color_manual(values=c("2"="gray","3"="#D55E00", "4"="mediumpurple4", "5"="#0072B2","6"="red","7"="lightskyblue2"))
-
-
-fig_1a <- ggplot() +
-  geom_point(data=factoral_eSTR_lrt_df, alpha=0.8,size=0.2,shape=20, aes(x=distance,y=padj,color=type)) +
-  geom_line(data=perc_eSTR,  aes(x=distance,y=padj),color="darkorange1",size=1)+
-  scale_color_manual(values=c("darkgray","lightskyblue2")) + 
-  geom_hline(data=perc_eSTR,aes(yintercept = bf_thres),
-             color = "red",
-             linetype=3 )+
-  theme_cust+
-  theme(legend.position = "none") +
-  labs(x="Distance to TSS (Mb)", 
-       color="")+
-  theme(legend.position = "none", panel.spacing = unit(2,"line")) + 
-  scale_x_continuous(expand = c(0 , 0.01) ) + 
-  scale_y_continuous( expand = c(0, 0) ,name = expression(-log[10](italic(p))), 
-                      sec.axis = sec_axis( trans=~./10, name="Mean percentile of\nsignificant STRs / 20 kb") )+
-  facet_grid(.~str_genotype,scales="free")
-
-
-
-fig_1a <- ggplot() +
-  geom_point(data=factoral_eSTR_lrt_log, alpha=0.8,size=0.2,shape=20, aes(x=distance,y=real_lrt_p )) +
-  #geom_line(data=perc_eSTR,  aes(x=distance,y=padj),color="darkorange1",size=1)+
- # scale_color_manual(values=c("darkgray","lightskyblue2")) + 
-  geom_hline(data=perc_eSTR,aes(yintercept = bf_thres),
-             color = "red",
-             linetype=3 )+
-  theme_cust+
-  theme(legend.position = "none") +
-  labs(x="Distance to TSS (Mb)", 
-       color="")+
-  theme(legend.position = "none", panel.spacing = unit(2,"line")) + 
-  scale_x_continuous(expand = c(0 , 0.01) ) + 
-  scale_y_continuous( expand = c(0, 0) ,name = expression(-log[10](italic(p))), 
-                      sec.axis = sec_axis( trans=~./10, name="Mean percentile of\nsignificant STRs / 20 kb") )+
-  facet_grid(nSTR_pheno~str_genotype,scales="free")
-
-
-fig_1a <- ggplot() +
-  geom_point(data=factoral_eSTR_lrt_log, alpha=0.8,size=0.2,shape=20, aes(x=distance,y=real_lrt_p ,color=factor(nSTR_pheno))) +
-  #geom_line(data=perc_eSTR,  aes(x=distance,y=padj),color="darkorange1",size=1)+
-  # scale_color_manual(values=c("darkgray","lightskyblue2")) + 
-  # geom_hline(data=perc_eSTR,aes(yintercept = bf_thres),
-  #            color = "red",
-  #            linetype=3 )+
     theme_cust+
   theme(legend.position = "none") +
   labs(x="Distance to TSS (Mb)", 
-       color="")+
+       y=expression(-log[10](italic(p))) )+
   theme(legend.position = "none", panel.spacing.x  = unit(2,"line")) + 
   scale_x_continuous(expand = c(0 , 0.01) ) + 
-  # scale_y_continuous( expand = c(0, 0) ,name = expression(-log[10](italic(p))), 
-  #                     sec.axis = sec_axis( trans=~./10, name="Mean percentile of\nsignificant STRs / 20 kb") )+
     facet_grid(nSTR_pheno~str_genotype,scales="free")+
   scale_color_manual(values=c("2"="gray","3"="#D55E00", "4"="mediumpurple4", "5"="#0072B2","6"="red","7"="lightskyblue2"))
+
+
+ggsave(fig_S1_new, filename = paste("../figures/supp_fig1_new.png",sep = ""), units = "mm",height = 160, width = 170)
 
 
 
@@ -192,7 +149,7 @@ fig_1b <- ggplot( ) +
   ylab("Variance explained by eSTRs")+
   xlab("Variance explained by local eQTL") +
   labs(color="Number of STR alleles") +
-  scale_color_manual(values=c("2"="gray","3"="#D55E00", "4"="mediumpurple4", "5"="#0072B2","6"="red"))
+  scale_color_manual(values=c("2"="gray","3"="#D55E00", "4"="mediumpurple4", "5"="#0072B2","6"="black"))
 
 ###### fig_1  ######
 
